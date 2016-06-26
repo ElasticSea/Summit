b7cf55b4bb64b1487de81ad4ec7f75e5f6a595f1
ElasticSea <info@elasticsea.com>
2016-06-27 01:52:28 +0200

    Adding light switching logic.

---
+++ Assets/Scripts/LightGrid.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Assets.Base.Scripts.Grid;
+using Assets.Shared.Scripts;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class LightGrid : MonoBehaviour
+    {
+        private Grid grid;
+
+        public int Range;
+
+        private void Start()
+        {
+            var children = transform.Children();
+            grid = GetComponent<Grid>();
+            children
+                .Select(child => child.GetComponent<LightSwitch>())
+                .ForEach(child => child.OnSwitchClicked += FlipSwitch);
+
+        }
+
+        private void FlipSwitch(LightSwitch lightSwitch)
+        {
+            var cell = lightSwitch.GetComponent<GridCell>();
+            for (var x = cell.X - Range; x <= cell.X + Range; x++)
+            {
+                for (var y = cell.Y - Range; y <= cell.Y + Range; y++)
+                {
+                    if (grid.InBounds(x, y))
+                    {
+                        grid[x,y].GetComponent<LightSwitch>().Switch();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Shared.Scripts;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Collider))]
+    public class LightSwitch : MonoBehaviour
+    {
+
+        public delegate void OnSwitchClickedHandler(LightSwitch lightSwitch);
+        public event OnSwitchClickedHandler OnSwitchClicked;
+
+        public int Levels = 1;
+        private int currentLevel;
+
+        public void Switch()
+        {
+            setLevel((currentLevel + 1) % (Levels + 1));
+        }
+
+        private void setLevel(int newLevel)
+        {
+            currentLevel = newLevel;
+            transform.SetY(currentLevel * 0.1f);
+        }
+
+        public void OnMouseDown()
+        {
+            if (OnSwitchClicked != null) OnSwitchClicked(this);
+        }
+    }
+}
