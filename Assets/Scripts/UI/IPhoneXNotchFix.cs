bea6c3d730ad70c16a674d26e3936af2a825e0de
ElasticSea <info@elasticsea.com>
2018-03-26 10:06:29 +0200

    Adding fix for iphone x notch.

---
+++ Assets/Scripts/UI/IPhoneXNotchFix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.iOS;
+
+namespace Assets.Scripts.UI
+{
+    public class IPhoneXNotchFix : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (Device.generation != DeviceGeneration.iPhoneX)
+            {
+                DestroyImmediate(gameObject);
+            }
+        }
+    }
+}
