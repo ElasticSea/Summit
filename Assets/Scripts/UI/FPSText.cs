37d4fde9eeb2a1a8576e389483f1392cd9c08f54
ElasticSea <info@elasticsea.com>
2018-04-08 12:43:01 +0200

    Adding fps script.

---
+++ Assets/Scripts/UI/FPSText.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Core.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class FPSText : MonoBehaviour
+    {
+        [SerializeField] private Text text;
+        [SerializeField] private FpsCounter fpsCounter;
+
+        private void Update()
+        {
+            text.text = fpsCounter.Fps.ToString("0.## Fps");
+        }
+    }
+}
