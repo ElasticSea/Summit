d10ea917bbb20ef637ffc7e1eee5f72081011b9c
ElasticSea <info@elasticsea.com>
2019-11-03 16:13:34 +0100

    Uncommited stuff.

---
+++ Assets/Scripts/CenterOfShit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CenterOfShit : MonoBehaviour
+    {
+        [SerializeField] private Transform[] points;
+
+        private void OnDrawGizmos()
+        {
+            var total = new Vector3();
+            foreach (var point in points)
+            {
+
+                total += point.position;
+            }
+           
+            Gizmos.DrawSphere(total / points.Length, .1f);
+        }
+    }
+}
